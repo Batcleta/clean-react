ec158817bd5b9b8c0b2f6121cd06c452d8861938
Batcleta <thewazig.wf@gmail.com>
2022-07-07 18:35:14 -0300

    test: ensuring that RemoteAuthentification returns the correct body object

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,34 @@
+// Todos os casos de Remote irão fazer requisições http
+import { mockAuthentification } from '../../../domain/test/mock-authentification'
+import { HttpPostClientSpy } from '../../test/mock-http-post-client'
+import { RemoteAuthentication } from './remote-authentication'
+
+type SutTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  // SUT - System under test
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+  return { sut, httpPostClientSpy }
+}
+
+describe('RemoteAuthentication', () => {
+  test('Should call HttpPostClient with correct URL', async () => {
+    const url = 'any_url'
+    const { sut, httpPostClientSpy } = makeSut()
+    await sut.auth(mockAuthentification())
+    // Testar se a implementação da abstração da requisição de post, que será injetada dentro de RemoteAuthentication
+    // está recebendo a utl que é passada como atributo para o construtor da classe RemoteAuthentication
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+
+  test('Should call HttpPostClient with correct body', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const body = mockAuthentification()
+    await sut.auth(body)
+    expect(httpPostClientSpy.body).toEqual(body)
+  })
+})
